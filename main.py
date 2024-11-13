2f355fe191419b52b256fef44c99f3cf39a9ed94
Krzysztof <Krzysztof@slm>
2024-11-13 15:07:18 +0100

    Player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+import player, circleshape
 
 def main():
     print("Starting asteroids!")
@@ -12,12 +13,14 @@ def main():
     clk = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         dt = clk.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player1.draw(screen)
         pygame.display.flip()
         
 
