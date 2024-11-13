b111c7fff46bc203a852c21ccb8d68bd61ca6eef
Krzysztof <Krzysztof@slm>
2024-11-13 13:08:42 +0100

    Added clock

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clk = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        dt = clk.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill((0, 0, 0))
         pygame.display.flip()
+        
+
 
 if __name__ == "__main__":
     main()
